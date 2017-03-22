4eb6263647da577fc05c228805c670ce4181cd15
orisano <owan.orisano@gmail.com>
2017-03-23 02:27:35 +0900

    refactor tree_encode

--- summpy/tools.py
+++ summpy/tools.py
@@ -7,16 +7,11 @@ import json
 
 
 def tree_encode(obj, encoding='utf-8'):
-    type_ = type(obj)
-    if type_ == list or type_ == tuple:
+    if isinstance(obj, (list, tuple)):
         return [tree_encode(e, encoding) for e in obj]
-    elif type_ == dict:
-        new_obj = dict(
-            (tree_encode(k, encoding), tree_encode(v, encoding))
-            for k, v in obj.iteritems()
-        )
-        return new_obj
-    elif type_ == unicode:
+    elif isinstance(obj, dict):
+        return {tree_encode(k, encoding): tree_encode(v, encoding) for k, v in obj.iteritems()}
+    elif isinstance(obj, unicode):
         return obj.encode(encoding)
     else:
         return obj
